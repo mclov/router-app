ed5ba418a0f1ece0cf9180b37ec009d5a5827780
Michael Clovis <michaelclovis@protonmail.com>
2019-12-10 09:23:27 -0800

    add params and css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Food from './Food';
+import { Route } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Food name='avocado' />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App" >
+        <Route exact path='/food/:name' render={(routeParams) => <Food {...routeParams} />} />
+      </div>
+    );
+  }
 }
 
 export default App;
