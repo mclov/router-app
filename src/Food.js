512cbe7fa46e5de70f6158f45c23ed849fb334d5
Michael Clovis <michaelclovis@protonmail.com>
2019-12-10 10:41:37 -0800

    validate search

--- src/Food.js
+++ src/Food.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import './Food.css';
 
 export class Food extends Component {
+
   render() {
     const name = this.props.match.params.name
     const url = `https://source.unsplash.com/1600x900/?${name}`;
 
     return (
       <div className='Food'>
-        <h1>I love to eat {name}</h1>
-        <img src={url} alt={name} />
+        {/\d/.test(name) ? (
+          <Redirect to='/' />
+        ) : (
+            <div>
+              <h1>I love to eat {name}</h1>
+              <img src={url} alt={name} />
+            </div>
+          )}
       </div>
     )
   }
