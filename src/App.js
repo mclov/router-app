d59a4f5f56d017a962276f254d5755d568edb75c
Michael Clovis <michaelclovis@protonmail.com>
2019-12-10 09:32:39 -0800

    add switch & 404 page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import Food from './Food';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App" >
-        <Route exact path='/food/:name' render={(routeParams) => <Food {...routeParams} />} />
+        <Switch>
+          <Route exact path='/food/:name' render={(routeParams) => <Food {...routeParams} />} />
+          <Route render={() => <h1>404 Page Not Found</h1>} />
+        </Switch>
       </div>
     );
   }
